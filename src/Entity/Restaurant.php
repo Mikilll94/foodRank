1299ffa7f39e5697acb2776bff8f21859af04397
Mikołaj Waśniewski <wasniewskimikolaj@gmail.com>
2017-12-10 00:00:28 +0100

    Database and first table created.

---
+++ src/Entity/Restaurant.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\RestaurantRepository")
+ */
+class Restaurant
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    private $image_url;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    private $short_description;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    private $description;
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function setName($name): void
+    {
+        $this->name = $name;
+    }
+
+    public function getImageUrl()
+    {
+        return $this->image_url;
+    }
+
+    public function setImageUrl($image_url): void
+    {
+        $this->image_url = $image_url;
+    }
+
+    public function getShortDescription()
+    {
+        return $this->short_description;
+    }
+
+    public function setShortDescription($short_description): void
+    {
+        $this->short_description = $short_description;
+    }
+
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    public function setDescription($description): void
+    {
+        $this->description = $description;
+    }
+}
