3d5b90ffa698e8bdf85fcb8325c10e3e4e7f5eb6
Mikołaj Waśniewski <wasniewskimikolaj@gmail.com>
2017-12-10 17:28:39 +0100

    Strange string interpolation

--- initial_data.sql
+++ initial_data.sql
@@ -1,12 +1,12 @@
 INSERT INTO restaurants (name, image_url, short_description, description)
-VALUES ('Tutti Santi', 'image1.jpg', 'Jedyna w Polsce pizzeria pod patronatem Mistrza Włoch i vice-mistrza świata. Valeria Valle.',
+VALUES ('Tutti Santi', 'image1.8e9b4ad0.jpg', 'Jedyna w Polsce pizzeria pod patronatem Mistrza Włoch i vice-mistrza świata. Valeria Valle.',
        'Long description');
 INSERT INTO restaurants (name, image_url, short_description, description)
-VALUES ('A nóż widelec', 'image1.jpg', 'Odwiedzając A nóż widelec, wkraczasz w wielowymiarowy świat smaków i aromatów kuchni wielkopolskiej jak również europejskiej.',
+VALUES ('A nóż widelec', 'image2.e9e95845.jpg', 'Odwiedzając A nóż widelec, wkraczasz w wielowymiarowy świat smaków i aromatów kuchni wielkopolskiej jak również europejskiej.',
        'Long description');
 INSERT INTO restaurants (name, image_url, short_description, description)
-VALUES ('Weranda Caffe', 'image1.jpg', 'Ukryta w samym centrum, tuż przy Starym Rynku, jest idealnie przytulna i zachwyca swym urokiem. Ta niewielka kawiarnia czaruje detalami – od kształtu filiżanki, giętych brzegów drewnianych stołów po zapach idealnie dobranych świeżych kwiatów. Na dziedzińcu schowany jest zaciszny ogród, w którym można zapomnieć o zgiełku miasta i zatopić się we wszechobecnej zieleni..',
+VALUES ('Weranda Caffe', 'image3.5122cb7d.jpg', 'Ukryta w samym centrum, tuż przy Starym Rynku, jest idealnie przytulna i zachwyca swym urokiem. Ta niewielka kawiarnia czaruje detalami – od kształtu filiżanki, giętych brzegów drewnianych stołów po zapach idealnie dobranych świeżych kwiatów. Na dziedzińcu schowany jest zaciszny ogród, w którym można zapomnieć o zgiełku miasta i zatopić się we wszechobecnej zieleni..',
        'Long description');
 INSERT INTO restaurants (name, image_url, short_description, description)
-VALUES ('Restauracja Papierówka', 'image1.jpg', 'Jedyna w Polsce pizzeria pod patronatem Mistrza Włoch i vice-mistrza świata. Valeria Valle.',
+VALUES ('Restauracja Papierówka', 'image4.5da1b52f.jpg', 'Jedyna w Polsce pizzeria pod patronatem Mistrza Włoch i vice-mistrza świata. Valeria Valle.',
        'Long description');
--- src/Controller/RestaurantController.php
+++ src/Controller/RestaurantController.php
@@ -1,9 +1,12 @@
 <?php
 namespace App\Controller;
 
+use App\Entity\Restaurant;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+use Symfony\Component\Debug\Debug;
 
 class RestaurantController extends Controller
 {
@@ -12,7 +15,12 @@ class RestaurantController extends Controller
     */
     public function Index()
     {
-        return $this->render('Restaurant/index.html.twig');
+        Debug::enable();
+        $restaurants = $this->getDoctrine()
+            ->getRepository(Restaurant::class)->findAll();
+
+        return $this->render('Restaurant/index.html.twig',
+            [ 'restaurants' => $restaurants ]);
     }
 
     /**
