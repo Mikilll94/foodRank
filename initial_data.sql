249a107cab940b7f7025233061db25d3b57ab150
Mikołaj Waśniewski <wasniewskimikolaj@gmail.com>
2017-12-11 17:57:28 +0100

    Proper images

--- initial_data.sql
+++ initial_data.sql
@@ -1,19 +1,19 @@
 INSERT INTO restaurants (name, image_url, short_description, description)
-VALUES ('Tutti Santi', 'image1.8e9b4ad0.jpg', 'Jedyna w Polsce pizzeria pod patronatem Mistrza Włoch i vice-mistrza świata. Valeria Valle.',
+VALUES ('Tutti Santi', 'image1.b42db6a8.jpg', 'Jedyna w Polsce pizzeria pod patronatem Mistrza Włoch i vice-mistrza świata. Valeria Valle.',
        'Mistrzowski smak
 Teraz pizza mistrza Włoch w Twoim mieście, a także wprost do Twojego domu.
 Naszym partnerem jest Valerio Valle, mistrz Włoch w pizzy klasycznej, jeden z najbardziej utytułowanych włoskich pizzaiolo, wielokrotnie nagradzany w międzynarodowych konkursach.
 Valerio Valle prowadzi regularne szkolenia w zakresie pizzy w Città Sant’ Angelo (PE). Aktualnie zajmuje wysoką pozycję w TOP 25 Włoskich Pizzaioli wg Roberto Piccinelli.');
 INSERT INTO restaurants (name, image_url, short_description, description)
-VALUES ('A nóż widelec', 'image2.e9e95845.jpg', 'Odwiedzając A nóż widelec, wkraczasz w wielowymiarowy świat smaków i aromatów kuchni wielkopolskiej jak również europejskiej.',
+VALUES ('A nóż widelec', 'image2.37c6d7f2.jpg', 'Odwiedzając A nóż widelec, wkraczasz w wielowymiarowy świat smaków i aromatów kuchni wielkopolskiej jak również europejskiej.',
        'Tutaj tradycja spotyka się z nowoczesnością, prezentując innowacyjną wizję kulinarnego dziedzictwa regionu. Czeka tu na Ciebie karta autorskich dań skomponowanych z bogactwa składników sezonowych, którymi cieszyć się możesz w przytulnie zaaranżowanym wnętrzu.');
 INSERT INTO restaurants (name, image_url, short_description, description)
-VALUES ('Weranda Caffe', 'image3.5122cb7d.jpg', 'Ukryta w samym centrum, tuż przy Starym Rynku, jest idealnie przytulna i zachwyca swym urokiem.',
+VALUES ('Weranda Caffe', 'image3.68bdeed1.jpg', 'Ukryta w samym centrum, tuż przy Starym Rynku, jest idealnie przytulna i zachwyca swym urokiem.',
        'Jak jednym zdaniem można opisać wszystkie restauracje Weranda Family? To połączenie niespotykanej dziś autentyczności, gościnności, designu oraz smaku serwowanych potraw. Wpadając do nas nawet na chwilę, ma się poczucie bycia we właściwym miejscu.
 Co nas wyróżnia? Wspólną cechą naszych lokali jest atmosfera – rodzinna, sielska, sprzyjająca spotkaniom. Budujemy ją nie tylko precyzyjnie dopracowanymi daniami i legendarnym, zmieniającym się wraz z porami roku wystrojem (połączeniem zieleni i ręcznie wykonanych ozdób), ale przede wszystkim ludźmi z prawdziwą pasją gotowania i tworzenia czegoś nowego.
 Każda z naszych restauracji jest inna, jednak wspólnie tworzą spójny koncept WERANDA FAMILY.');
 INSERT INTO restaurants (name, image_url, short_description, description)
-VALUES ('Restauracja Papierówka', 'image4.5da1b52f.jpg', 'Jedyna w Polsce pizzeria pod patronatem Mistrza Włoch i vice-mistrza świata. Valeria Valle.',
+VALUES ('Restauracja Papierówka', 'image4.be1a5d23.jpg', 'Jedyna w Polsce pizzeria pod patronatem Mistrza Włoch i vice-mistrza świata. Valeria Valle.',
        'Papierówka to zielona odskocznia w centrum miasta. U nas zjesz lekką kuchnię regionalną urozmaiconą sezonowo, odpoczniesz w cieniu parku Zakrzewskiego oraz uraczysz się soczystymi jabłkami. Jesteśmy rodziną i naszą pasją od wielu lat jest gotowanie dla innych, przygotowywane dania przyrządzamy z najlepszych produktów z lokalnych gospodarstw.
 Starujemy już od 9.00 z pysznymi śniadaniami które dadzą wam energię na cały dzień, zapraszamy również na lunche i obiady.
 Papierówka to nie tylko smakowite dania ale również świetny wystrój: prosty świeży i przyjazny, już od progu poczęstuj się jabłkiem i rozpocznij przygodę z papierówką.');
