ea4dbb561d2a0c4e04dbfb798a99a8e02efdbd64
Mikołaj Waśniewski <wasniewskimikolaj@gmail.com>
2017-12-14 00:17:57 +0100

    TODO: star rating sending by POST

--- src/Controller/RestaurantController.php
+++ src/Controller/RestaurantController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 use App\Entity\Restaurant;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
@@ -34,30 +35,32 @@ class RestaurantController extends Controller
     /**
     * @Route("restaurants/{restaurant_id}", name="restaurant_details", requirements={"restaurant_id"="\d+"})
     */
-    public function Details($restaurant_id)
+    public function Details($restaurant_id, Request $request)
     {
         $restaurant = $this->getDoctrine()
             ->getRepository(Restaurant::class)
             ->find($restaurant_id);
 
         $form = $this->createFormBuilder()
-//            ->add('rate', ChoiceType::class, array(
-//                'choices' => array(
-//                    '1' => '1',
-//                    '2' => '2',
-//                    '3' => '3',
-//                    '4' => '4',
-//                    '5' => '5'
-//                ),
-//                'expanded'    => true,
-//                'multiple' => false,
-//            ))
+            ->add('rate', ChoiceType::class, array(
+                'choices' => array(
+                    '1' => '1',
+                    '2' => '2',
+                    '3' => '3',
+                    '4' => '4',
+                    '5' => '5'
+                ),
+                'expanded' => true,
+                'multiple' => false,
+            ))
             ->add('content', TextareaType::class)
             ->getForm();
 
+        $form->handleRequest($request);
+
         return $this->render('Restaurant/details.html.twig', array(
             'restaurant' => $restaurant,
-            'form' => $form->createView(),
+            'form' => $form->createView()
         ));
     }
 }
