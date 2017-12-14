30f554c82b5ec27ab3899f3d27ff651facd6cf3b
Mikołaj Waśniewski <wasniewskimikolaj@gmail.com>
2017-12-14 22:26:46 +0100

    Saving comments to database

--- src/Controller/RestaurantController.php
+++ src/Controller/RestaurantController.php
@@ -1,19 +1,22 @@
 <?php
+
 namespace App\Controller;
 
+use App\Entity\Comment;
 use App\Entity\Restaurant;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Validator\Constraints\DateTime;
 
 class RestaurantController extends Controller
 {
     /**
-    * @Route("/", name="all_restaurants")
-    * @Route("restaurants")
-    */
+     * @Route("/", name="all_restaurants")
+     * @Route("restaurants")
+     */
     public function Index()
     {
         $restaurants = $this->getDoctrine()
@@ -21,20 +24,20 @@ class RestaurantController extends Controller
             ->findAll();
 
         return $this->render('Restaurant/index.html.twig',
-            [ 'restaurants' => $restaurants ]);
+            ['restaurants' => $restaurants]);
     }
 
     /**
-    * @Route("restaurants/create")
-    */
+     * @Route("restaurants/create")
+     */
     public function Create()
     {
         return $this->render('Restaurant/create.html.twig');
     }
 
     /**
-    * @Route("restaurants/{restaurant_id}", name="restaurant_details", requirements={"restaurant_id"="\d+"})
-    */
+     * @Route("restaurants/{restaurant_id}", name="restaurant_details", requirements={"restaurant_id"="\d+"})
+     */
     public function Details($restaurant_id, Request $request)
     {
         $restaurant = $this->getDoctrine()
@@ -57,6 +60,21 @@ class RestaurantController extends Controller
             ->getForm();
 
         $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $form_data = $form->getData();
+
+            $comment = new Comment();
+            $comment->setAuthor('Anonymous');
+            $comment->setRate($form_data['rate']);
+            $comment->setContent($form_data['content']);
+            $comment->setPostingDate(new \DateTime('now'));
+            $comment->setRestaurant($restaurant);
+            $em->persist($comment);
+            $em->flush();
+        }
+
 
         return $this->render('Restaurant/details.html.twig', array(
             'restaurant' => $restaurant,
