b242f9b38cd6a922c98c9bbf996f63393ec78451
Mikołaj Waśniewski <wasniewskimikolaj@gmail.com>
2017-12-17 22:35:49 +0100

    More initial data

--- assets/js/restaurant_index.js
+++ assets/js/restaurant_index.js
@@ -3,3 +3,7 @@ require('../images/image1.jpg');
 require('../images/image2.jpg');
 require('../images/image3.jpg');
 require('../images/image4.jpg');
+require('../images/image5.jpg');
+require('../images/image6.jpg');
+require('../images/image7.jpg');
+require('../images/image8.jpg');
--- initial_data.sql
+++ initial_data.sql
@@ -17,6 +17,30 @@ VALUES ('Restauracja Papierówka', 'image4.jpg', 'Jedyna w Polsce pizzeria pod p
        'Papierówka to zielona odskocznia w centrum miasta. U nas zjesz lekką kuchnię regionalną urozmaiconą sezonowo, odpoczniesz w cieniu parku Zakrzewskiego oraz uraczysz się soczystymi jabłkami. Jesteśmy rodziną i naszą pasją od wielu lat jest gotowanie dla innych, przygotowywane dania przyrządzamy z najlepszych produktów z lokalnych gospodarstw.
 Starujemy już od 9.00 z pysznymi śniadaniami które dadzą wam energię na cały dzień, zapraszamy również na lunche i obiady.
 Papierówka to nie tylko smakowite dania ale również świetny wystrój: prosty świeży i przyjazny, już od progu poczęstuj się jabłkiem i rozpocznij przygodę z papierówką.');
+INSERT INTO restaurants (name, image_url, short_description, description)
+VALUES ('Restauracja Ratuszova', 'image5.jpg', 'Restauracja Ratuszova serdecznie zaprasza w swoje progi wszystkich, którzy lubią lub pragną zasmakować tradycyjne potrawy kuchni polskiej!',
+        'W naszym menu na szczególną uwagę zasługuje czernina z domowym makaronem, kaczka pieczona z jabłkami oraz dania z dziczyzny. Miłośników Slow Food, pasjonatów zdrowego odżywiania oraz wszystkich smakoszy w szczególności zachęcamy do spróbowania dań gotowanych innowacyjną metodą sous-vide. Dla jeszcze większego urozmaicenia nasze menu uzupełniliśmy smakami kuchni międzynarodowej. W naszej restauracji  dbamy o to by sezonowo zmieniać potrawy oraz wystrój sal. Niezmienną natomiast częścią RATUSZOVEJ jest wysoka jakość produktów, których używamy, ich oryginalność oraz miła i profesjonalna obsługa, która wprawi Państwa w wyjątkowy nastrój i zadba aby wizyta była niezapomnianą przygodą zarówno kulinarną jak i towarzyską.');
+INSERT INTO restaurants (name, image_url, short_description, description)
+VALUES ('Figaro', 'image6.jpg', 'Przez żołądek do serca ?  Dlatego przez żołądek trafiło tu wielu !',
+        'Warto, bo w kulinarny świat wkładamy całe nasze serce, czego odbiciem są potrawy które serwujemy .
+
+Wierzymy, że  owoce naszej pasji podane na talerzu i przyprawione z włoską finezją pozwolą zdobyć Tych, na których zależy nam tak bardzo, że zapraszamy Ich do Figaro.
+
+Gotowanie bierzemy na siebie, by cała reszta była już prosta.
+
+Znakomita kuchnia, wystrój i atmosfera pobudzą wszystkie zmysły. Dbamy o szczegóły, bo jedzenie po włosku to nie tylko wrażenia smakowe, lecz także estetyczne i słuchowe.
+
+U nas się je ... rozmawia ... gestykuluje.
+
+W Figaro jedzeniem się żyje !');
+INSERT INTO restaurants (name, image_url, short_description, description)
+VALUES ('Muga Restauracja', 'image7.jpg', 'Położona w bezpośredniej okolicy poznańskiego Starego Rynku restauracja MUGA jest idealnym miejscem spotkań osób, które cenią sobie dobrą i dopracowaną kuchnię.',
+        'Położona w bezpośredniej okolicy poznańskiego Starego Rynku restauracja MUGA jest idealnym miejscem spotkań osób, które cenią sobie dobrą i dopracowaną kuchnię. Gościom proponujemy autorskie kreacje uwzględniające sezonowość oraz europejskie trendy. Szczególną uwagę skupiamy na estetyce podania i harmonijnym łączeniu smaków, dzięki czemu posiłek w MUGA staje się ucztą dla wszystkich zmysłów. Restauracja Muga połączona jest z winiarnią Casa de Vinos , która w swoim portfolio posiada ponad 300 etykiet win z całego świata . Mamy nadzieję, że nasze zaangażowanie oraz doświadczenie zyskają Państwa uznanie. Serdecznie zapraszamy do restauracji MUGA.');
+INSERT INTO restaurants (name, image_url, short_description, description)
+VALUES ('Cucina 88', 'image8.jpg', 'W Cucinie można zobaczyć smaki, usłyszeć zapachy i dotknąć świeżości.',
+        'W Cucinie można zobaczyć smaki, usłyszeć zapachy i dotknąć świeżości. Poczuć tętno Kuchni – bo ona tu żyje. Jesteśmy ludźmi, którzy tę Kuchnię tworzą i naturą, której się podporządkowują. To natura wyznacza bowiem pojawiające się w karcie propozycje. Menu zmienia się wraz z porami roku. Inaczej smakuje lato od jesieni, czego innego potrzebujemy wiosną, czym innym obdarza nas zima. Zdajemy sobie z tego sprawę, dlatego w Cucinie dania są przygotowane wyłącznie ze świeżych, sezonowych produktów. Pragniemy rozpowszechniać ideę „Wiem, Co Jem”, dlatego Goście w naszych delikatesach mogą kupić składniki, na kanwie których powstały ich ulubione posiłki. A przede wszystkim zapytać kucharzy, jak z nich skorzystać. Bo w prawdziwej Kuchni dużo rozmawia się o tym, co nas syci i daje rozkosz.');
+
+
 
 INSERT INTO comments (restaurant_id, author, content, rate, posting_date)
 VALUES ((SELECT id from restaurants WHERE name='Tutti Santi'), 'Gosiaddd', 'Pyszna pizza, miła i sprawna obsługa, krótki czas oczekiwania na zamówienie :) Miejsce godne polecenia. Niestety brakuje mi wegańskiej pizzy :)',
