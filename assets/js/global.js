dc442829bcd90176d55c6d218f338031ab87e069
Mikołaj Waśniewski <wasniewskimikolaj@gmail.com>
2017-12-22 21:04:47 +0100

    CSS-es - adornment

--- assets/js/global.js
+++ assets/js/global.js
@@ -2,7 +2,6 @@ require('../css/global.scss');
 require('../images/throbber.gif');
 require('../css/footer.scss');
 require('../css/navbar.scss');
-require('../images/user.svg');
 
 const $ = require('jquery');
 global.$ = global.jQuery = $;
