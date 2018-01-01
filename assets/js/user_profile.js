07a57ecb4cd8f34c87a99b5f9913d6887ab845ab
Mikołaj Waśniewski <wasniewskimikolaj@gmail.com>
2018-01-01 23:44:38 +0100

    Bootstrap file input - improvements

--- assets/js/user_profile.js
+++ assets/js/user_profile.js
@@ -4,25 +4,19 @@ require('../images/user-thumb.jpg');
 
 $(document).ready(function () {
     $('#myModal').on('show.bs.modal', function () {
-        let btnCust = '<button type="button" class="btn btn-secondary" title="Add picture tags" ' +
-            'onclick="alert(\'Call your custom code here.\')">' +
-            '<i class="glyphicon glyphicon-tag"></i>' +
-            '</button>';
-        // $("#avatar-2").fileinput({
-        //     overwriteInitial: true,
-        //     maxFileSize: 1500,
-        //     showClose: false,
-        //     showCaption: false,
-        //     showBrowse: false,
-        //     browseOnZoneClick: true,
-        //     removeLabel: '',
-        //     removeIcon: '<i class="glyphicon glyphicon-remove"></i>',
-        //     removeTitle: 'Cancel or reset changes',
-        //     elErrorContainer: '#kv-avatar-errors-2',
-        //     msgErrorClass: 'alert alert-block alert-danger',
-        //     defaultPreviewContent: `<img src="${$('#avatar_img').prop('src')}"><h6 class="text-muted">Kliknij aby wybrać</h6>`,
-        //     layoutTemplates: {main2: '{preview} ' +  btnCust + ' {remove} {browse}'},
-        //     allowedFileExtensions: ["jpg", "png", "gif"]
-        // });
+        $("#avatar-input").fileinput({
+            language: 'pl',
+            overwriteInitial: true,
+            maxFileSize: 1500,
+            showClose: false,
+            showCaption: false,
+            showRemove: false,
+            showCancel: false,
+            removeLabel: '',
+            elErrorContainer: '#kv-avatar-errors-2',
+            msgErrorClass: 'alert alert-block alert-danger',
+            defaultPreviewContent: `<img src="${$('#avatar_img').prop('src')}"><h6 class="text-muted">Kliknij aby wybrać</h6>`,
+            allowedFileExtensions: ["jpg", "png", "gif"]
+        });
     });
 });
