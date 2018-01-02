c0694e6173bfc8c599c36bd2670f3a549544b72a
Mikołaj Waśniewski <wasniewskimikolaj@gmail.com>
2018-01-02 15:55:39 +0100

    Deleting unused avatars.

--- src/Controller/FileUploadController.php
+++ src/Controller/FileUploadController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Comment;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Security\Core\User\UserInterface;
@@ -19,8 +20,14 @@ class FileUploadController extends Controller
      */
     public function changeAvatar(Request $request, UserInterface $user)
     {
+        $fs = new Filesystem();
+
+        if ($user->getAvatarName()) {
+            $fs->remove('uploads/avatars/' . $user->getAvatarName());
+        }
+
         $file = $request->files->get('file');
-        $fileName = md5(uniqid()).'.'.$file->guessExtension();
+        $fileName = md5(uniqid()) . '.' . $file->guessExtension();
 
         $file->move($this->getParameter('avatars_directory'), $fileName);
 
