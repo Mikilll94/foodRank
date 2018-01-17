be845159c9ca017a5079ac0935c59e5154ffb58c
Mikołaj Waśniewski <wasniewskimikolaj@gmail.com>
2018-01-17 23:15:15 +0100

    Login throbber

--- assets/js/login.js
+++ assets/js/login.js
@@ -3,6 +3,9 @@ const alertify = require('alertifyjs');
 $('#login-nav').submit(function(e) {
     e.preventDefault();
 
+    $('#login-via-navbar').hide();
+    $('#login-via-navbar-throbber').show();
+
     let username = $('#username').val();
     let password = $('#password').val();
     let loginData = {
@@ -16,9 +19,13 @@ $('#login-nav').submit(function(e) {
         dataType: 'json',
         data: JSON.stringify(loginData),
         success: function () {
+            $('#login-via-navbar-throbber').hide();
+            $('#login-via-navbar').show();
             location.reload();
         },
         error: function (xhr, ajaxOptions, thrownError) {
+            $('#login-via-navbar-throbber').hide();
+            $('#login-via-navbar').show();
             alertify.set('notifier','position', 'top-left');
             if (thrownError === "Unauthorized") {
                 alertify.notify('Nieprawidłowe hasło lub login', 'error', 5);
