77b17461cd48736417367ffa2ba9e476eb8ae9c6
Mikołaj Waśniewski <wasniewskimikolaj@gmail.com>
2018-01-18 20:22:13 +0100

    Button throbbers refactoring
    Squashed commits:
    
    [9187c58] next

--- assets/js/global.js
+++ assets/js/global.js
@@ -19,8 +19,8 @@ $(window).on('load', function() {
 $(document).ready(function() {
 
     $('#reset-password-btn').click(function () {
-        $('#reset-password').hide();
-        $('#reset-password-throbber').show();
+        let $resetPasswordBtn = $(this);
+        $resetPasswordBtn.button('loading');
         $.ajax({
             url: '/forgot_password',
             type: 'POST',
@@ -29,8 +29,7 @@ $(document).ready(function() {
                 email: $('#email_address_forgot').val()
             },
             success: function (data) {
-                $('#reset-password-throbber').hide();
-                $('#reset-password').show();
+                $resetPasswordBtn.button('reset');
                 if (data.errors.length > 0) {
                     alertify.set('notifier', 'position', 'top-left');
                     alertify.notify(data.errors.join('\n'), 'error', 5);
--- assets/js/login.js
+++ assets/js/login.js
@@ -1,37 +1,36 @@
 const alertify = require('alertifyjs');
 
-$('#login-nav').submit(function(e) {
-    e.preventDefault();
+$(document).ready(function () {
+    $('#login-nav').submit(function(e) {
+        e.preventDefault();
 
-    $('#login-via-navbar').hide();
-    $('#login-via-navbar-throbber').show();
+        $('#login-navbar-btn').button('loading');
 
-    let username = $('#username').val();
-    let password = $('#password').val();
-    let loginData = {
-        username: username,
-        password: password
-    };
-    $.ajax({
-        url: '/login_json',
-        type: 'POST',
-        contentType: 'application/json',
-        dataType: 'json',
-        data: JSON.stringify(loginData),
-        success: function () {
-            $('#login-via-navbar-throbber').hide();
-            $('#login-via-navbar').show();
-            location.reload();
-        },
-        error: function (xhr, ajaxOptions, thrownError) {
-            $('#login-via-navbar-throbber').hide();
-            $('#login-via-navbar').show();
-            alertify.set('notifier','position', 'top-left');
-            if (thrownError === "Unauthorized") {
-                alertify.notify('Nieprawidłowe hasło lub login', 'error', 5);
-            } else {
-                alertify.notify('Wystąpił nieoczekiwany błąd', 'error', 5);
+        let username = $('#username').val();
+        let password = $('#password').val();
+        let loginData = {
+            username: username,
+            password: password
+        };
+        $.ajax({
+            url: '/login_json',
+            type: 'POST',
+            contentType: 'application/json',
+            dataType: 'json',
+            data: JSON.stringify(loginData),
+            success: function () {
+                $('#login-navbar-btn').button('reset');
+                location.reload();
+            },
+            error: function (xhr, ajaxOptions, thrownError) {
+                $('#login-navbar-btn').button('reset');
+                alertify.set('notifier','position', 'top-left');
+                if (thrownError === "Unauthorized") {
+                    alertify.notify('Nieprawidłowe hasło lub login', 'error', 5);
+                } else {
+                    alertify.notify('Wystąpił nieoczekiwany błąd', 'error', 5);
+                }
             }
-        }
-    })
+        })
+    });
 });
