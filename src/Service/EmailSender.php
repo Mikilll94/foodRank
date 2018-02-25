dc95635f1772b33fd0a20bb0deab1280ff65b677
Mikołaj Waśniewski <wasniewskimikolaj@gmail.com>
2018-02-25 14:44:05 +0100

    Email fix

--- src/Service/EmailSender.php
+++ src/Service/EmailSender.php
@@ -5,6 +5,13 @@ namespace App\Service;
 class EmailSender
 {
     /**
+     * EmailSender constructor.
+     */
+    public function __construct()
+    {
+    }
+
+    /**
      * @param $subject
      * @param $template
      * @param $to
@@ -14,15 +21,19 @@ class EmailSender
         $https['ssl']['verify_peer'] = false;
         $https['ssl']['verify_peer_name'] = false;
 
+        $sender_email = 'portal.foodrank@gmail.com';
+
         $transport = (new \Swift_SmtpTransport('smtp.gmail.com', '587', 'tls'))
-            ->setUsername('wasniewskimikolaj@gmail.com')
-            ->setPassword('svwhlfeixxsjxaoy')
+            ->setUsername($sender_email)
+            ->setPassword('foodrankportal123')
             ->setStreamOptions($https);
 
         $message = (new \Swift_Message($subject))
-            ->setFrom('no-reply@foodrank.com')
+            ->setFrom($sender_email)
             ->setTo($to)
-            ->setBody($template);
+            ->setBody($template)
+            ->setContentType('text/html')
+            ->setCharset('utf-8');
         (new \Swift_Mailer($transport))->send($message);
     }
 }
